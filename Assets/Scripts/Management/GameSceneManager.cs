e1451dee172ea32b3f1798419b4f4b9e5605ed77
fabricio-cesur <fabricio.s399523@cesurformacion.com>
2026-01-27 11:19:41 +0100

    INIT: Cambio de nombre
    Se cambia de nombre el archivo de SceneManager a GameSceneManager para
    que no haya conflictos o confusiones con el SceneManager nativo de
    Unity.

---
+++ Assets/Scripts/Management/GameSceneManager.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+#if UNITY_EDITOR
+    using UnityEditor;
+#endif
+
+public class GameSceneManager : MonoBehaviour
+{
+    public string escenaJuego; //nombre de la escena del Juego
+    public string escenaMenu; //nombre de la escena del menu
+
+    public void IrMenu()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(escenaMenu);
+    }
+    public void IrJuego()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(escenaJuego);
+    }
+
+    public void SalirDelJuego()
+    {
+    //si está en el editor de unity parar el juego
+    #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+    #else
+    //si está abierto cómo aplicación, cerrar el juego
+            Application.Quit();
+    #endif
+    }
+}
