9490961149362fb9799805ff55b9346f39b5cfac
fabricio-cesur <fabricio.s399523@cesurformacion.com>
2026-04-10 19:02:50 +0200

    CHANGE: Borrando bloque de código comentado

--- Assets/Scripts/NPC/Pinchos.cs
+++ Assets/Scripts/NPC/Pinchos.cs
@@ -15,14 +15,5 @@ public class Pinchos : MonoBehaviour
             Debug.Log($"PINCHOS: Daño a {otro.gameObject.name} de {danoPincho} puntos.");
             damageable.RecibirDano(danoPincho);
         }
-        // if (tag == "Player" || tag == "Enemy")
-        // {
-        //     IDamageable damageable = otroObjeto.GetComponent<IDamageable>();
-        //     if (damageable != null)
-        //     {
-        //         Debug.Log($"PINCHOS: Daño a {otro.gameObject.name} de {danoPincho} puntos.");
-        //         damageable.RecibirDano(danoPincho);
-        //     }
-        // }
     }
 }
