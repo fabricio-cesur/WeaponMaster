8329b8c791f27a7a11e5edfd07eb365ccc524fef
fabricio-cesur <fabricio.s399523@cesurformacion.com>
2026-05-19 21:52:50 +0200

    FEAT: Sistema de Perfiles
    Se ha terminado todo el sistema de perfiles para tener varias partidas
    guardadas junto con todo lo de la UI para que esté correctamente. Además
    de eso se cambio todo el sistema de guardados para que pueda funcionar
    estos cambios.

--- Assets/Scripts/Management/MenuManager.cs
+++ Assets/Scripts/Management/MenuManager.cs
@@ -1,15 +1,16 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuManager : MonoBehaviour
 {
     [Header("Botones de Interfaz")]
-    public GameObject btnPlay;       // Tu BtnPlay original
-    public GameObject btnContinue;   // Tu nuevo BtnContinue
-    public GameObject btnNewGame;    // Tu nuevo BtnNewGame
+    public GameObject btnPlay;
+    public GameObject btnContinue;
+    public GameObject btnNewGame;
+    public GameObject btnExit;
 
     void Start()
     {
-        // 1. Buscamos el panel padre por su nombre exacto
         GameObject panelBotones = GameObject.Find("Buttons");
 
         if (panelBotones == null)
@@ -18,31 +19,32 @@ public class MenuManager : MonoBehaviour
             return;
         }
 
-        // 2. Buscamos a los hijos dentro del panel (esto encuentra objetos aunque estén apagados)
-        // Usamos el ? por seguridad, por si escribes mal un nombre
         btnPlay = panelBotones.transform.Find("BtnPlay")?.gameObject;
         btnContinue = panelBotones.transform.Find("BtnContinue")?.gameObject;
         btnNewGame = panelBotones.transform.Find("BtnNewGame")?.gameObject;
-        GameObject btnExit = panelBotones.transform.Find("BtnExit")?.gameObject;
+        btnExit = panelBotones.transform.Find("BtnExit")?.gameObject;
 
-        // 3. Conectamos los clics por código automáticamente (¡Magia!)
-        if (btnPlay != null) btnPlay.GetComponent<UnityEngine.UI.Button>().onClick.AddListener(BotonJugar);
-        if (btnContinue != null) btnContinue.GetComponent<UnityEngine.UI.Button>().onClick.AddListener(BotonContinuar);
-        if (btnNewGame != null) btnNewGame.GetComponent<UnityEngine.UI.Button>().onClick.AddListener(BotonNuevaPartida);
-        if (btnExit != null) btnExit.GetComponent<UnityEngine.UI.Button>().onClick.AddListener(BotonSalir);
+        if (btnPlay != null) btnPlay.GetComponent<Button>().onClick.AddListener(BotonJugar);
+        if (btnContinue != null) btnContinue.GetComponent<Button>().onClick.AddListener(BotonContinuar);
+        if (btnNewGame != null) btnNewGame.GetComponent<Button>().onClick.AddListener(BotonNuevaPartida);
+        if (btnExit != null) btnExit.GetComponent<Button>().onClick.AddListener(BotonSalir);
 
-        // 4. Lógica de mostrar/ocultar botones según la partida guardada
+        // Llamamos a la actualización visual al arrancar
+        ActualizarBotonesInterfaz();
+    }
+
+    // Nueva función pública que el ProfileManager podrá llamar al cambiar de perfil
+    public void ActualizarBotonesInterfaz()
+    {
         if (GameManager.gm != null && GameManager.gm.ExistePartidaGuardada())
         {
             if (btnPlay != null) btnPlay.SetActive(false);
-            
             if (btnContinue != null) btnContinue.SetActive(true);
             if (btnNewGame != null) btnNewGame.SetActive(true);
         }
         else
         {
             if (btnPlay != null) btnPlay.SetActive(true);
-            
             if (btnContinue != null) btnContinue.SetActive(false);
             if (btnNewGame != null) btnNewGame.SetActive(false);
         }
@@ -61,16 +63,14 @@ public class MenuManager : MonoBehaviour
     }
 
     public void BotonNuevaPartida()
+{
+    if (GameManager.gm != null)
     {
-        string ruta = Application.persistentDataPath + "/partida.json";
-        if (System.IO.File.Exists(ruta)) 
-        {
-            System.IO.File.Delete(ruta);
-        }
-        
-        if (GameManager.gm != null) GameManager.gm.ReiniciarDatosGuardados();
-        GameSceneManager.instance.IrJuego(); 
+        GameManager.gm.ReiniciarDatosGuardados();
+        GameManager.gm.GuardarPartidaEnDisco(); 
     }
+    GameSceneManager.instance.IrJuego(); 
+}
 
     public void BotonSalir()
     {
